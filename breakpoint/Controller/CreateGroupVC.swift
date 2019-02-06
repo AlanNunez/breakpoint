1ff105b562f167a52061b9337932fd13ee336e24
Alan Nunez <raul.nunez01@gmail.com>
2019-02-06 15:50:20 -0400

    Creating UserCell

--- breakpoint/Controller/CreateGroupVC.swift
+++ breakpoint/Controller/CreateGroupVC.swift
@@ -19,9 +19,28 @@ class CreateGroupVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     @IBAction func closeBtnWasPressed(_ sender: Any) {
     }
     @IBAction func doneBtwnWasPressed(_ sender: Any) {
+    }
+}
+
+extension CreateGroupVC: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "userCell") as? UserCell else { return UITableViewCell() }
+        let profileImage = UIImage(named: "defaultProfileImage")
+        cell.configureCell(profileImage: profileImage!, email: "marty@bttf.com", isSelected: true)
+        return cell
     }
 }
---
+++ breakpoint/Controller/UserCell.swift
@@ -0,0 +1,31 @@
+
+//
+//  UserCell.swift
+//  breakpoint
+//
+//  Created by Desarrollo on 2/6/19.
+//  Copyright © 2019 Dev Core. All rights reserved.
+//
+
+import UIKit
+
+class UserCell: UITableViewCell {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var checkImage: UIImageView!
+    
+    func configureCell(profileImage image: UIImage, email: String, isSelected: Bool) {
+        self.profileImage.image = image
+        self.emailLbl.text = email
+        if isSelected {
+            self.checkImage.isHidden = false
+        } else {
+            self.checkImage.isHidden = true
+        }
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
